4a8a69d69fda360f80b6032256f3a1fb6019088e
OkamotoReiji <220310b@ugs.kochi-tech.ac.jp>
2020-12-27 15:32:36 +0900

    update : add insert function

--- server/DB/src/calorie_set.c
+++ server/DB/src/calorie_set.c
@@ -3,31 +3,65 @@
 #include <string.h>
 #include <mysql/mysql.h>
 int main(void){
-  MYSQL *conn     = NULL;
-  MYSQL_RES *resp = NULL;
-  MYSQL_ROW row;
-  char sql_str[255];
-  char *sql_serv  = "localhost";
-  char *user      = "root";
-  char *passwd    = "mariadb";
-  char *db_name   = "jogging";
+    calorie_insert();
 
-  memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
+    return 0;
+}
 
-  // mysql接続
-  conn = mysql_init(NULL);
-  if( !mysql_real_connect(conn,sql_serv,user,passwd,db_name,0,NULL,0) ){
+void calorie_update() {
+    MYSQL *conn     = NULL;
+    MYSQL_RES *resp = NULL;
+    MYSQL_ROW row;
+    char sql_str[255];
+    char *sql_serv  = "localhost";
+    char *user      = "root";
+    char *passwd    = "mariadb";
+    char *db_name   = "jogging";
+
+    memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
+
+    // mysql接続
+    conn = mysql_init(NULL);
+    if( !mysql_real_connect(conn,sql_serv,user,passwd,db_name,0,NULL,0) ){
     // error
     exit(-1);
-  }
+    }
 
-  // クエリ実行
-  snprintf( &sql_str[0] , sizeof(sql_str)-1 , "update CALORIE_TABLE SET food_calorie where food=1" );
-  if( mysql_query( conn , &sql_str[0] ) ){
+    // クエリ実行
+    snprintf( &sql_str[0] , sizeof(sql_str)-1 , "update CALORIE_TABLE SET food_calorie=909 where food=1" );
+    if( mysql_query( conn , &sql_str[0] ) ){
     // error
     mysql_close(conn);
     exit(-1);
-  }
-  mysql_close(conn);
-  return 0;
+    }
+    mysql_close(conn);
+}
+
+void calorie_insert() {
+    MYSQL *conn     = NULL;
+    MYSQL_RES *resp = NULL;
+    MYSQL_ROW row;
+    char sql_str[255];
+    char *sql_serv  = "localhost";
+    char *user      = "root";
+    char *passwd    = "mariadb";
+    char *db_name   = "jogging";
+
+    memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
+
+    // mysql接続
+    conn = mysql_init(NULL);
+    if( !mysql_real_connect(conn,sql_serv,user,passwd,db_name,0,NULL,0) ){
+    // error
+    exit(-1);
+    }
+
+    // クエリ実行
+    snprintf( &sql_str[0] , sizeof(sql_str)-1 , "INSERT into CALORIE_TABLE values(1, 'ラーメン', 436)" );
+    if( mysql_query( conn , &sql_str[0] ) ){
+    // error
+    mysql_close(conn);
+    exit(-1);
+    }
+    mysql_close(conn);
 }
