e26be51bceaa5b880abb9313fff353db728e8978
MikioKobayashi <mhkoba.kke@gmail.com>
2021-01-01 14:47:37 +0900

    user_set&usedlog_set touhougou

--- server/DB/src/user.c
+++ server/DB/src/user.c
@@ -2,35 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <mysql/mysql.h>
-void user_update();
+//#include <mysql/mysql.h>
+#include <jogging.h>
 
-typedef struct {
-    int id;
-    char loginname[10];
-    char pass[32];
-    char name[96];
-    float weight;
-    float height;
-    int age;
-    int sex;
-    char birth[11];
-    float gweight;
-    char term[11];
-    char mail[30];
-   } UT;
-
-UT user_select(int id) {
+USER_TABLE user_select(int id) {
     MYSQL *conn     = NULL;
     MYSQL_RES *resp = NULL;
     MYSQL_ROW row;
-    char sql_str[255];
+    char sql_str[511];
     char *sql_serv  = "localhost";
     char *user      = "root";
     char *passwd    = "mariadb";
     char *db_name   = "jogging";
 
-    UT res_data;
+    USER_TABLE res_data;
 
     memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
 
@@ -65,8 +50,8 @@ UT user_select(int id) {
         res_data.age = atoi(row[2]);
         res_data.sex = atoi(row[3]);
         sprintf(res_data.birth, "%s", row[4]); 
-        res_data.gweight = atof(row[5]);
-        sprintf(res_data.term, "%s", row[6]);
+        res_data.gwoal_eight = atof(row[5]);
+        sprintf(res_data.goal_term, "%s", row[6]);
     }
 
     // 後片づけ
@@ -77,22 +62,22 @@ UT user_select(int id) {
 }
 
 //更新
-void user_update(UT ut){
-  MYSQL *conn     = NULL;
-  char sql_str[255];
-  char *sql_serv  = "localhost";
-  char *user      = "root";
-  char *passwd    = "mariadb";
-  char *db_name   = "jogging";
+int user_update(USER_TABLE ut){
+    MYSQL *conn     = NULL;
+    char sql_str[511];
+    char *sql_serv  = "localhost";
+    char *user      = "root";
+    char *passwd    = "mariadb";
+    char *db_name   = "jogging";
+    
+    memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
 
-  memset( &sql_str[0] , 0x00 , sizeof(sql_str) );
-
-  // mysql接続
-  conn = mysql_init(NULL);
-  if( !mysql_real_connect(conn,sql_serv,user,passwd,db_name,0,NULL,0) ){
-      // error
-      exit(-1);
-  }
+    // mysql接続
+    conn = mysql_init(NULL);
+    if( !mysql_real_connect(conn,sql_serv,user,passwd,db_name,0,NULL,0) ){
+        // error
+        retrun 1;
+    }
 
     // クエリ実行
     char id_buf[9];
@@ -100,19 +85,20 @@ void user_update(UT ut){
     snprintf( &sql_str[0] , sizeof(sql_str)-1 , "UPDATE USER_TABLE\
     SET weight = %3.1f, height = %3.1f, age = %d, goal_weight = %3.1f, goal_term = '%s', mail_address = '%s'\
     where id = '%s'" \
-            , ut.weight, ut.height, ut.age, ut.gweight, ut.term, ut.mail, id_buf);
+            , ut.weight, ut.height, ut.age, ut.goal_weight, ut.goal_term, ut.mail_address, id_buf);
     if( mysql_query( conn , &sql_str[0] ) ){
         // error
         mysql_close(conn);
-        exit(-1);
+        return 1;
     }
-
-  // 後片づけ
-  mysql_close(conn);
+    
+    // 後片づけ
+    mysql_close(conn);
+    return 0;
 }
 
 //追加
-void user_insert(UT ut) {
+int user_insert(USER_TABLE ut) {
     MYSQL *conn     = NULL;
     char sql_str[511];
     char *sql_serv  = "localhost";
@@ -132,7 +118,7 @@ void user_insert(UT ut) {
     char id_buf[9];
     snprintf(id_buf, 9, "%08d", ut.id);
     sprintf(sql_str, "INSERT INTO USER_TABLE VALUES('%08d', '%s', '%s', '%s', %lf, %lf, %d, %d, '%s', %lf, '%s', '%s')"\
-          , ut.id, ut.loginname, ut.pass, ut.name, ut.weight, ut.height, ut.age, ut.sex, ut.birth, ut.gweight, ut.term, ut.mail);
+          , ut.id, ut.loginname, ut.pass, ut.name, ut.weight, ut.height, ut.age, ut.sex, ut.birth, ut.goal_weight, ut.goal_term, ut.mail_address);
     if( mysql_query( conn , &sql_str[0] ) ){
         // error
         mysql_close(conn);
@@ -140,13 +126,16 @@ void user_insert(UT ut) {
     }
     //後片付け
     mysql_close(conn);
+    return res_data;
 }
 
+/*
 //追加:テスト用
 int main(){
     // クエリ実行
-    //UT ut = {3, "MJ", "makeinu", "マケイヌ・ジャクサン", 64.0, 168.7, 45, 1, "2009-06-25", 45.0, "2021/04/01", "goodloser@goodluck.hg"};
-    UT ut = {3, "MJ", "makeinu", "マケイヌ・ジャクサン", 64.0, 168.7, 45, 1, "2009-06-25", 45.0, "2025/12/31", "goodloser@goodluck.hg"};
+    //USER_TABLE ut = {3, "MJ", "makeinu", "マケイヌ・ジャクサン", 64.0, 168.7, 45, 1, "2009-06-25", 45.0, "2021/04/01", "goodloser@goodluck.hg"};
+    USER_TABLE ut = {3, "MJ", "makeinu", "マケイヌ・ジャクサン", 64.0, 168.7, 45, 1, "2009-06-25", 45.0, "2025/12/31", "goodloser@goodluck.hg"};
     //user_insert(ut, sql_str);
     user_update(ut);
 }
+*/
