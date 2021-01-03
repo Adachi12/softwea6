abe8cd888f0e87582a8197909733e06ec07f0586
OkamotoReiji <220310b@ugs.kochi-tech.ac.jp>
2021-01-03 21:36:17 +0900

    update some files

--- server/main/submain.c
+++ server/main/submain.c
@@ -8,27 +8,34 @@ int main() {
 }
 
 
+
 void *access_table(request r) {
+    CALORIE_TABLE *resp_data_cal;
+    SAVED_ROUTE_TABLE *resp_data_srt;
+    USEDLOG_TABLE *resp_data_ult;
+    USER_TABLE *resp_data_ut;
+
+    CALORIE_TABLE test_data_cal;
+    SAVED_ROUTE_TABLE test_data_srt;
+    USEDLOG_TABLE test_data_ult;
+    USER_TABLE test_data_ut;
+
     switch (r.access_target) {
         calorie_table:
             switch (r.op) {
                 select_t:
-                    CALORIE_TABLE *resp_data;
-                    resp_data = calorie_select();
-                    return resp_data;
-                default:
-                    break;
+                    resp_data_cal = calorie_select();
+                    return resp_data_cal;
             }
         saved_route_table:
-            SAVED_ROUTE_TABLE test_data = {
+            test_data_srt = {
                 0, 0,
                 "", "", "", "", ""
             };
             switch (r.op) {
                 select_t:
-                    SAVED_ROUTE_TABLE *resp_data;
-                    resp_data = saved_route_select();
-                    return resp_data;
+                    resp_data_srt = saved_route_select();
+                    return resp_data_srt;
                 insert_t:
                     return saved_route_insert(test_data);
                 update_t:
@@ -37,14 +44,13 @@ void *access_table(request r) {
                     break;
             }
         usedlog_table:
-            USEDLOG_TABLE test_data = {
+            test_data_ult = {
                 0, 0, "0000/00/00 00:00:00",
                 0, "00:00:00", 0
             };
             switch (r.op) {
                 select_t:
-                    USEDLOG_TABLE *resp_data;
-                    resp_data = usedlog_select();
+                    resp_data_ult = usedlog_select();
                     return resp_data;
                 insert_t:
                     return usedlog_insert(test_data);
@@ -54,15 +60,14 @@ void *access_table(request r) {
                     break;
             }
         user_table:
-            USEDLOG_TABLE test_data = {
+            test_data_ut = {
                 0, 0, "", "", "", 0.0, 0.0, 
                 0, 0, "", 0.0, "", ""
             };
             switch (r.op) {
                 select_t:
-                    USER_TABLE *resp_data;
-                    resp_data = user_select();
-                    return resp_data;
+                    resp_data_ut = user_select();
+                    return resp_data_ut;
                 insert_t:
                     return user_insert(test_data);
                 update_t:
