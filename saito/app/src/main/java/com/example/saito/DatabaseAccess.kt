760e4766a33882bba4896eb851410bc3a18d8180
BUNWEMON <y.bunwemon@docomo.ne.jp>
2021-01-20 16:06:24 +0900

    yes

--- saito/app/src/main/java/com/example/saito/DatabaseAccess.kt
+++ saito/app/src/main/java/com/example/saito/DatabaseAccess.kt
@@ -5,6 +5,7 @@ import java.nio.*
 import java.io.*
 
 // import saito.*
+// close()
 
 enum class DataBase(val id: Int) {
     CALORIE_TABLE(0x00),
@@ -32,6 +33,20 @@ fun main(args: Array<String>) {
     recvBuf = db.recvMsg()
     println(recvBuf)
 }
+
+class CALORIE(val name: String, val calorie: String) {
+    //println()
+}
+
+class USEDLOG(val date: String, val mileage: String, val time: String, val calorie: String) {
+}
+
+class USER(val id: String, val name: String, val weight: String, val height: String,
+           val age: String, val sex: String, val birth: String, val goal_weight: String,
+           val goal_term: String, val mail_address: String) {
+
+}
+
 ​
 class JogDB : Socket("ec2-54-90-205-238.compute-1.amazonaws.com", 60000, null, 0) {
     private val request_stream: OutputStream = this.getOutputStream()
@@ -40,7 +55,6 @@ class JogDB : Socket("ec2-54-90-205-238.compute-1.amazonaws.com", 60000, null, 0
     ​
     // 引数3つ
     fun sendMsg(table: Int, operation: Int, data: String): Int {
-        // val hed = (table shl 4) + operation
         val hed = table + operation
         val msg: String = hed.toString() + "\n" + data
         val charset = Charsets.UTF_8
@@ -57,7 +71,6 @@ class JogDB : Socket("ec2-54-90-205-238.compute-1.amazonaws.com", 60000, null, 0
 
     // 引数2つ
     fun sendMsg(table: Int, operation: Int): Int {
-        // val hed = (table shl 4) + operation
         val hed = table + operation
         val msg: String = hed.toString()
         val charset = Charsets.UTF_8
@@ -72,16 +85,72 @@ class JogDB : Socket("ec2-54-90-205-238.compute-1.amazonaws.com", 60000, null, 0
         return 0
     }
     ​
-    fun recvMsg() : String {
+    fun recvMsg() : Int {
         val charset = Charsets.UTF_8
         var resp_buf = ByteArray(buf_size) { 0 }
         ​
         try {
             response_stream.read(resp_buf, 0, buf_size)
         } catch (e: IOException) {
-            return ""
+            return 1
         }
+
+        return 0
+    }
+
+    fun recvMsgCalorie() : CALORIE {
+        val charset = Charsets.UTF_8
+        var resp_buf = ByteArray(buf_size) { 0 }
         ​
-        return resp_buf.toString(charset)
+        try {
+            response_stream.read(resp_buf, 0, buf_size)
+        } catch (e: IOException) {
+            return CALORIE("失敗","失敗")
+        }
+
+        val str = resp_buf.toString(charset)
+        val str_list = str.split("\n")
+        val resp_data = CALORIE(str_list.get(0), str_list.get(1))
+
+        return resp_data
+    }
+
+    fun recvMsgUsedlog() : List<List<String>> {
+        val charset = Charsets.UTF_8
+        var resp_buf = ByteArray(buf_size) { 0 }
+        ​
+        try {
+            response_stream.read(resp_buf, 0, buf_size)
+        } catch (e: IOException) {
+            return USEDLOG()
+        }
+
+        val recData: List<String> = resp_buf.toString().split("\n")
+        var returnData: List<List<String>> =
+                recData.map { it.split("\n").map{ it.removePrefix("[")} }
+        returnData.map { it.map{println("$it\n")} }
+        ​
+        return returnData
+    }
+
+    fun recvMsgUser() : USER {
+        val charset = Charsets.UTF_8
+        var resp_buf = ByteArray(buf_size) { 0 }
+        ​
+        try {
+            response_stream.read(resp_buf, 0, buf_size)
+        } catch (e: IOException) {
+            return USER("失敗", "失敗", "失敗", "失敗",
+                    "失敗", "失敗", "失敗", "失敗",
+                    "失敗", "失敗")
+        }
+
+        val str = resp_buf.toString(charset)
+        val str_list = str.split("\n")
+        val resp_data = USER(str_list.get(0), str_list.get(1), str_list.get(2), str_list.get(3),
+                str_list.get(4), str_list.get(5), str_list.get(6), str_list.get(7),
+                str_list.get(8), str_list.get(9))
+
+        return resp_data
     }
 }
