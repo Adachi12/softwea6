7581cdd0f3f9fe274ebafbd9358f9f5f4c80ce8b
Adachi12 <adaken1119@icloud.com>
2021-01-27 22:37:25 +0900

    統合ファイル

---
+++ 統合ファイル/MyApplication4/app/src/main/java/com/example/myapplication/DirectionsParser.kt
@@ -0,0 +1,98 @@
+/*
+ * Useful to track user
+ *
+ */
+package com.example.myapplication
+
+import com.google.android.gms.maps.model.LatLng
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.lang.Double
+import java.util.*
+
+/**
+ * Created by NgocTri on 12/11/2017.
+ */
+
+class DirectionsParser {
+    /**
+     * Returns a list of lists containing latitude and longitude from a JSONObject
+     */
+    fun parse(jObject: JSONObject): List<List<HashMap<String, String>>> {
+        val routes: MutableList<List<HashMap<String, String>>> = ArrayList()
+        var jRoutes: JSONArray? = null
+        var jLegs: JSONArray? = null
+        var jSteps: JSONArray? = null
+        try {
+            jRoutes = jObject.getJSONArray("routes")
+
+            // Loop for all routes
+            for (i in 0 until jRoutes.length()) {
+                jLegs = (jRoutes[i] as JSONObject).getJSONArray("legs")
+                val path: MutableList<*> = ArrayList<HashMap<String, String>>()
+
+                //Loop for all legs
+                for (j in 0 until jLegs.length()) {
+                    jSteps = (jLegs[j] as JSONObject).getJSONArray("steps")
+
+                    //Loop for all steps
+                    for (k in 0 until jSteps.length()) {
+                        var polyline = ""
+                        polyline = ((jSteps[k] as JSONObject)["polyline"] as JSONObject)["points"] as String
+                        val list = decodePolyline(polyline)
+
+                        //Loop for all points
+                        for (l in list.indices) {
+                            val hm = HashMap<String, String>()
+                            hm["lat"] = Double.toString((list[l] as LatLng).latitude)
+                            hm["lon"] = Double.toString((list[l] as LatLng).longitude)
+                            path.add(hm as Nothing)
+                        }
+                    }
+                }
+            }
+        } catch (e: JSONException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+        }
+        return routes
+    }
+
+    /**
+     * Method to decode polyline
+     * Source : http://jeffreysambells.com/2010/05/27/decoding-polylines-from-google-maps-direction-api-with-java
+     */
+    private fun decodePolyline(encoded: String): List<*> {
+        val poly: MutableList<*> = ArrayList<Any?>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].toInt() - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+            val p = LatLng(lat.toDouble() / 1E5,
+                    lng.toDouble() / 1E5)
+            poly.add(p as Nothing)
+        }
+        return poly
+    }
+}
